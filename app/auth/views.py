f3d0a948463a2fe3c856068a611aa0dc070f118a
Rômulo Sousa Brito de Melo <PT3020487@IFSPROOT.PTB>
2024-03-28 20:23:55 -0300

    finish

--- app/auth/views.py
+++ app/auth/views.py
@@ -25,21 +25,43 @@ def unconfirmed():
     return render_template('auth/unconfirmed.html')
 
 
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import auth
+from ..models import User
+from .forms import LoginForm, RegistrationForm
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    # Insira o código aqui
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and check_password_hash(user.password_hash, form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)
 
 @auth.route('/logout')
 @login_required
 def logout():
-    # Insira o código aqui
-
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    # Insira o código aqui
-
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password_hash=generate_password_hash(form.password.data))
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
 
 @auth.route('/confirm/<token>')
 @login_required
@@ -57,7 +79,7 @@ def confirm(token):
 @auth.route('/confirm')
 @login_required
 def resend_confirmation():
-    token = current_user.generate_confirmation_token()    
+    token = current_user.generate_confirmation_token()
     flash('<p>Para confirmar sua conta <a href="' + url_for('auth.confirm', token=token, _external=True) + '">clique aqui</a></p>')
     return redirect(url_for('main.index'))
 
